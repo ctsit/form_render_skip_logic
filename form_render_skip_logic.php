aca4fa6241b4784b59a03dd555f4e6b376dd934f
Sreeja <sreejakannagundl@ufl.edu>
2017-03-30 10:58:45 -0400

    modify form_render_skip_logic to disable union of forms and enable desired forms for patient in record_status_dashboard

--- form_render_skip_logic.php
+++ form_render_skip_logic.php
@@ -5,155 +5,159 @@
 //use json_encode? to port REDCap::getData output to JS
 //check for instruments of concern on returned json/JS object and enable their links
 return function() {
-    $SDH_type = REDCap::getData(13,'json',null,null,1,null,false,false,false,'[patient_type] = "1"',null,null);
-    //print $SDH_type;
-    $SAH_type = REDCap::getData(13,'json',null,null,1,null,false,false,false,'[patient_type] = "2"',null,    null);
-    $UBI_type = REDCap::getData(13,'json',null,null,1,null,false,false,false,'[patient_type] = "3"',null,    null);
-    ?>
-    <script>
-    console.log("hello");
-    var patient_types = {
-        "sdh" : {"type": <?php echo $SDH_type ?>, "records": []},
-        "sah" : {"type": <?php echo $SAH_type ?>, "records": []},
-        "ubi" : {"type": <?php echo $UBI_type ?>, "records": []}
-    };
-    function pop_records(patient_types) {
-        for (var patient_type in patient_types) {
-            //console.log(Object.values(type));
-            var patient_json = patient_types[patient_type];
-            //var record = patient_json[type];
-            for (var record in patient_json["type"]) {
-               patient_json["records"].push(patient_json["type"][record].unique_id); 
-            }
-            console.log(patient_json.records);
-        }
-    }
-    pop_records(patient_types);
-    $('document').ready(function(){
-        var json = [
-          { "action":"form_render_skip_logic",
-            "instruments_to_show" : [
-                {"logic":"[visit_1_arm_1][patient_type] = '1'",
-                 "instrument_names": ["sdh_details", "past_medical_history_sah_sdh"]},
-                {"logic":"[visit_1_arm_1][patient_type] = '2'",
-                 "instrument_names": ["sah_details", "past_medical_history_sah_sdh"]},
-                {"logic":"[visit_1_arm_1][patient_type] = '3'",
-                 "instrument_names": ["medications_sah_sdh"]}
-                ]
-            }
-        ];
 
-        render_form_skip_logic(json);
+    $SDH_type = REDCap::getData(12,'json',null,null,1,null,false,false,false,'[patient_type] = "1"',null,null);
+
+    $SAH_type = REDCap::getData(12,'json',null,null,1,null,false,false,false,'[patient_type] = "2"',null,    null);
+
+    $UBI_type = REDCap::getData(12,'json',null,null,1,null,false,false,false,'[patient_type] = "3"',null,    null);
+    ?>
+
+    <script>
+        var json = [{
+            "action": "form_render_skip_logic",
+            "instruments_to_show": [{
+                    "logic": "[visit_1_arm_1][patient_type] = '1'",
+                    "instrument_names": ["sdh_details", "past_medical_history_sah_sdh"]
+                },
+                {
+                    "logic": "[visit_1_arm_1][patient_type] = '2'",
+                    "instrument_names": ["sah_details", "past_medical_history_sah_sdh"]
+                },
+                {
+                    "logic": "[visit_1_arm_1][patient_type] = '3'",
+                    "instrument_names": ["medications_sah_sdh"]
+                }
+            ]
+        }];
+
+        var patient_types = {
+            "sdh": {
+                "type": <?php echo $SDH_type ?>,
+                "records": []
+            },
+            "sah": {
+                "type": <?php echo $SAH_type ?>,
+                "records": []
+            },
+            "ubi": {
+                "type": <?php echo $UBI_type ?>,
+                "records": []
+            }
+        };
+
+        function pop_records(patient_types) {
+            for (var patient_type in patient_types) {
+                var patient_json = patient_types[patient_type];
+                for (var record in patient_json["type"]) {
+                    patient_json["records"].push(patient_json["type"][record].unique_id);
+                }
+            }
+        }
+
+        function disableUnionOfForms(json) {
+            for (var names in json[0].instruments_to_show) {
+                for (var form in json[0].instruments_to_show[names].instrument_names) {
+                    var form_to_disable = json[0].instruments_to_show[names].instrument_names[form];
+                    disableFormsWithProp(form_to_disable);
+                }
+            }
+        }
+
+        function enableDesiredForms(json, patient_types) {
+            var index = 0;
+            for (var type in patient_types) {
+                var record = patient_types[type].records;
+                for (var rec in record) {
+                    for (var instrument in json[0].instruments_to_show[index].instrument_names) {
+                        enableFormsForPatientId(record[rec], json[0].instruments_to_show[index].instrument_names[instrument]);
+                    }
+                }
+                index++;
+            }
+        }
+
+        function enableFormsForPatientId(id, form) {
+            var rows = document.querySelectorAll('#record_status_table tbody tr');
+            var reg = new RegExp(form);
+
+            for (var i = 0; i < rows.length; i++) {
+                if (rows[i].cells[0].innerText == id) {
+                    for (var j = 0; j < rows[i].cells.length; j++) {
+                        if (reg.test(rows[i].cells[j].firstElementChild.href)) {
+                            enableForm(rows[i].cells[j]);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
 
         function render_form_skip_logic(json) {
 
             // Check the current url to make sure you are on the record status dashboard page
-            if(!/record_status_dashboard/.test(document.URL)) {
+            if (!/record_status_dashboard/.test(document.URL)) {
                 console.log("render_form_skip_logic is not running!");
                 return null;
-            }
-            else {
-            console.log("it is running!");
+            } else {
+                console.log("it is running!");
             }
 
             //check if we recieved the right json
-            if(!json[0].hasOwnProperty("action") && json[0]["action"] === "form_render_skip_logic") {
-                console.log("render_form_skip_logic is not running due to a json error");
+            if (!json[0].hasOwnProperty("action") && json[0]["action"] === "form_render_skip_logic") {
+                console.log("render_form_skip_logic is not running due to a invalid json object");
                 return null;
             }
-            
-            //check if we only want to hide a certain elements, defaults to hiding everything
-            if(json[0].hasOwnProperty("instruments_to_hide")) {
-               for(var i = 0; i < json[0]["instruments_to_hide"].length; i++) {
-                    disableLinksWithProp(json[0]["instruments_to_hide"][i]);
-               } 
-            } else {
-                disableAllLinks();
-            }
-            
-            if(json[0].hasOwnProperty("instruments_to_show")) {
-                for(var index in json[0]["instruments_to_show"]) {
-                    var pair = json[0]["instruments_to_show"][index];
-                    if(pair.logic){
-                            console.log(pair.instrument_names);
-                        for(var instrument in pair.instrument_names) {
-                            console.log('enableing ' + pair.instrument_names[instrument]);
-                            enableLinksWithProp(pair.instrument_names[instrument]);
-                        }
-                    } 
-                    //go through each cell
-                    //if right visit_and_arm and right patient_type
-                    //then enable all instruments in instruments_to_show
+
+            //check if we only want to hide a certain elements, defaults to hiding union of instruments to show
+            if (json[0].hasOwnProperty("instruments_to_hide")) {
+                for (var i = 0; i < json[0]["instruments_to_hide"].length; i++) {
+                    disableFormsWithProp(json[0]["instruments_to_hide"][i]);
                 }
+            } else {
+                disableUnionOfForms(json);
+            }
+
+            if (json[0].hasOwnProperty("instruments_to_show")) {
+                enableDesiredForms(json, patient_types);
             }
         }
 
-        //disables all links within the record table.
-        function disableAllLinks() {
-            var rows = document.querySelectorAll('#record_status_table tbody tr');
-            
-            for(var i = 0; i < rows.length;i++) {
-                for(var j = 0; j < rows[i].cells.length; j++) {
-                    disableLink(rows[i].cells[j]);
-                }
-            }
-        } 
-
-        function disableLink(cell) {
+        function disableForm(cell) {
             cell.firstElementChild.style.pointerEvents = 'none';
-            if(cell.firstElementChild.firstElementChild) {
+            if (cell.firstElementChild.firstElementChild) {
                 cell.firstElementChild.firstElementChild.style.opacity = '.1';
             }
         }
 
-        function enableLink(cell) {
+        function enableForm(cell) {
             cell.firstElementChild.style.pointerEvents = 'auto';
-            if(cell.firstElementChild.firstElementChild) {
+            if (cell.firstElementChild.firstElementChild) {
                 cell.firstElementChild.firstElementChild.style.opacity = '1';
             }
         }
-                
 
-
-        //enables all links with the given property inside their link name
-        function enableLinksWithProp(property) {
+        function disableFormsWithProp(property) {
 
             var rows = document.querySelectorAll('#record_status_table tbody tr');
             var reg = new RegExp(property);
 
-            for(var i = 0; i < rows.length;i++) {
-                for(var j = 0; j < rows[i].cells.length; j++) {
+            for (var i = 0; i < rows.length; i++) {
+                for (var j = 0; j < rows[i].cells.length; j++) {
                     var link = rows[i].cells[j].firstElementChild.href;
 
-                    if(reg.test(link)) {
-                        enableLink(rows[i].cells[j]);
+                    if (reg.test(link)) {
+                        disableForm(rows[i].cells[j]);
                     }
                 }
-            }    
+            }
         }
-    
-    
-        //disables all links with the given property inside their link name
-        function disableLinksWithProp(property) {
 
-            var rows = document.querySelectorAll('#record_status_table tbody tr');
-            var reg = new RegExp(property);
-
-            for(var i = 0; i < rows.length;i++) {
-                for(var j = 0; j < rows[i].cells.length; j++) {
-                    var link = rows[i].cells[j].firstElementChild.href;
-
-                    if(reg.test(link)) {
-                        disableLink(rows[i].cells[j]);
-                    }
-                }
-            }    
-        }
-    }
-);
-</script>
-<?php
+        $('document').ready(function() {
+            pop_records(patient_types);
+            render_form_skip_logic(json);
+        });
+    </script>
+    <?php
 }
 ?>
-
-
