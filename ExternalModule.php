8e5403cd8e0c6d4a3627e13d7afa056e133bf55c
Dileep <rajputd@ufl.edu>
2018-06-27 09:50:03 -0400

    Add method that converts FRSL v2.x.x settings to v3.x.x

--- ExternalModule.php
+++ ExternalModule.php
@@ -532,4 +532,66 @@ class ExternalModule extends AbstractExternalModule {
        return $settings;
      }
 
+    /**
+     * converts settings from from FRSL_v2.X to FRSL_v3.X
+     * @param array $old_settings, array of v2 settings indexed by project_id
+     * @return array $new_settings, array of v3 settings indexed by project_id
+     */
+     function convert2XSettingsTo3XSettings($old_settings) {
+       $module_id = $this->getFRSLModuleId();
+
+       //use old_settings to create new_settings
+       $new_settings = [];
+       foreach ($old_settings as $project_id => $old_setting) {
+
+         //generate some of the new settings using the old "instrument_name" values
+         $old_instrument_names = json_decode($old_setting["instrument_name"]);
+         $old_instrument_count = count($old_instrument_names);
+         $branching_logic = [];
+         $condition_operator = [];
+         $target_forms = [];
+         $target_events_select = [];
+         $target_events = [];
+
+         for($i = 0; $i < $old_instrument_count; $i++) {
+           $branching_logic[] = true;
+           $condition_operator[] = null;
+           $target_forms[] = [$old_instrument_names[$i]];
+           $target_events_select[] = false;
+           $target_events[] = [null];
+         }
+
+         //convert to nested JSON-arrays for storage
+         $branching_logic = "[" . json_encode($branching_logic) . "]";
+         $condition_operator = "[" . json_encode($condition_operator) . "]";
+         $target_forms = "[" . json_encode($target_forms) . "]";
+         $target_events_select =  "[" . json_encode($target_events_select) . "]";
+         $target_events = "[" . json_encode($target_events) . "]";
+
+         //create sub-structure for project setting
+         $setting = [];
+
+         /*added extra angle brackets around some values because every new config
+          is stored as a JSON-array or as a nested JSON-array*/
+         $setting["control_fields"] = $old_setting["control_field"];
+         $setting["control_mode"] = '["default"]';
+         $setting["control_event_id"] = $old_setting["event_name"];
+         $setting["control_field_key"] = $old_setting["field_name"];
+         $setting["control_piping"] = "[null]";
+         $setting["control_default_value"] = "[null]";
+         $setting["branching_logic"] = $branching_logic;
+         $setting["condition_value"] = "[" . $old_setting["control_field_value"] . "]";
+         $setting["condition_operator"] = $condition_operator;
+         $setting["target_forms"] = $target_forms;
+         $setting["target_events_select"] = $target_events_select;
+         $setting["target_events"] = $target_events;
+
+         //store in main data structure
+         $new_settings[$project_id] = $setting;
+       }
+
+       return $new_settings;
+
+     }
+
 }
